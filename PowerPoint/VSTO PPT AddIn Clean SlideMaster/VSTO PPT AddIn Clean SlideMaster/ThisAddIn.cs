8ef5d6445b7226577442015ba62e4036c77793d6
Piyush K Singh <piyush.singh1@vodafone.com>
2025-10-23 07:05:53 +0100

    First commit. Added ribbon section.

---
+++ PowerPoint/VSTO PPT AddIn Clean SlideMaster/VSTO PPT AddIn Clean SlideMaster/ThisAddIn.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Office.Core;
+
+namespace VSTO_PPT_AddIn_Clean_SlideMaster
+{
+    public partial class ThisAddIn
+    {
+        private void ThisAddIn_Startup(object sender, System.EventArgs e)
+        {
+        }
+
+        private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
+        {
+        }
+
+        #region VSTO generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InternalStartup()
+        {
+            this.Startup += new System.EventHandler(ThisAddIn_Startup);
+            this.Shutdown += new System.EventHandler(ThisAddIn_Shutdown);
+        }
+
+        #endregion
+
+        protected override IRibbonExtensibility CreateRibbonExtensibilityObject()
+        {
+            return new SlideMasterRibbon();
+        }
+    }
+}
